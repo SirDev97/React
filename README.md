1e1faffc5aee04333c59bf523100f24272921abb
Alkin Maystorov <alkin971@gmail.com>
2021-08-06 16:07:55 +0200

    add Multiple Components branch

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Dynamic Values in Templates Branch
+# Multiple Components Branch
+
+1. App.jsx - Root Component
+
+- Possible nested components:
+  - Navbar.jsx
+
+  - BlogDetails.jsx
+
+  - Sidebar.jsx
--- dojo-blog/src/App.jsx
+++ dojo-blog/src/App.jsx
@@ -1,25 +1,13 @@
 import "./App.css";
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
-  const title = "Welcome to the new blog";
-  const likes = 50;
-  // const character = { name: "yoshi", age: 20 };
-  const link = "http://www.google.com";
-
   return (
     <div className='App'>
+      <Navbar />
       <div className='content'>
-        <h1> {title} </h1>
-        <p>Liked {likes} times</p>
-
-        {/* <p>{character}</p> */}
-
-        <p>{10}</p>
-        <p>{"hello hackers"}</p>
-        <p>{[1, 2, 3]}</p>
-        <p>{Math.random() * 10}</p>
-
-        <a href={link}>Google Site</a>
+        <Home />
       </div>
     </div>
   );
