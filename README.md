771728ccaf877ccaedd41379ff6e74abb38bef97
Alkin Maystorov <alkin971@gmail.com>
2021-08-07 14:36:05 +0200

    Fetching Data with useEffect

--- README.md
+++ README.md
@@ -1,11 +1,1 @@
-# Using JSON Server
-
-- Endpoints
-
-  - /blogs - GET - Fetch all blogs
-
-  - /blogs/{id} - GET - Fetch a single blog
-
-  - /blogs - POST - Add a new blog
-
-  - /blogs/{id} - DELETE - Delete a blog
+# Fetching Data with useEffect
--- dojo-blog/src/Home.jsx
+++ dojo-blog/src/Home.jsx
@@ -2,25 +2,24 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
-    { title: "Web dev tips", body: "lorem ipsum...", author: "luigi", id: 3 },
-  ]);
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-
-    setBlogs(newBlogs);
-  };
+  const [blogs, setBlogs] = useState(null);
 
   useEffect(() => {
-    console.log("use effect ran");
+    fetch("http://localhost:5000/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setBlogs(data);
+      });
   }, []);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      {blogs && (
+        <BlogList blogs={blogs} title="All Blogs" /> // conditional templating
+      )}
     </div>
   );
 };
