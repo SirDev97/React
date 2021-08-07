6f5dc5b91259de1a26f968876535c2aed8f0e4d7
Alkin Maystorov <alkin971@gmail.com>
2021-08-07 15:05:42 +0200

    add Handling Fetch Errors

--- dojo-blog/src/Home.jsx
+++ dojo-blog/src/Home.jsx
@@ -4,21 +4,30 @@ import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost:5000/blogs")
       .then((res) => {
+        if (!res.ok) {
+          throw Error("Could not fetch the data for that resource");
+        }
         return res.json();
       })
       .then((data) => {
-        console.log(data);
         setBlogs(data);
         setIsLoading(false);
+        setError(null);
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        setError(err.message);
       });
   }, []);
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {isLoading && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
